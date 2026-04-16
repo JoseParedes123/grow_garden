6260834eb5fe2b73347a9b79579acf439bd7aa2b
benj4-PK <latellabenja2009@gmail.com>
2026-04-15 21:38:55 -0300

    diseño "beta"

---
+++ game.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body class="game-page">
+
+    <header>
+        <table border="0" width="100%" cellspacing="0" cellpadding="10">
+            <tr>
+                <td>
+                    <button id="Shop" style="background-color: #facc15;" onmouseover="this.style.backgroundColor='#16a34a'" onmouseout="this.style.backgroundColor='#facc15'"><a href="">tienda</a></button>
+                </td>
+
+                <td>
+                    <button id="Vender" style="background-color: #facc15;" onmouseover="this.style.backgroundColor='#16a34a'" onmouseout="this.style.backgroundColor='#facc15'"><a href="">vender</a></button>
+                </td>
+                <td>
+                    <button id="Profile" style="background-color: #facc15;" onmouseover="this.style.backgroundColor='#16a34a'" onmouseout="this.style.backgroundColor='#facc15'"><a href="">perfil</a></button>
+                </td>
+                <td>
+                    <button id="" style="background-color: #facc15;" onmouseover="this.style.backgroundColor='#16a34a'" onmouseout="this.style.backgroundColor='#facc15'"><a href="">lorem</a></button>
+
+                </td>
+                <td>
+                    <button id="" style="background-color: #facc15;" onmouseover="this.style.backgroundColor='#16a34a'" onmouseout="this.style.backgroundColor='#facc15'"><a href="">lorem</a></button>
+                </td>
+                <td>
+                    <!--aqui va el dinero del usuario-->
+                </td>
+            </tr>
+        </table>
+    </header>
+
+    <main class="cesped">
+        <section class="terreno">
+            
+            <div class="Arboles">
+
+                <img src="pngs/etapa5.png" alt="arbol">
+
+            </div>
+        </section>
+    </main>
+    
+</body>
+</html>
